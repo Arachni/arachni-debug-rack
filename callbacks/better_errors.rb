3df4b6196d28c384e8a4f856a8bf27daf13984c0
Tasos Laskos <tasos.laskos@gmail.com>
2017-01-06 16:25:36 +0200

    Updated copyright notices

--- callbacks/better_errors.rb
+++ callbacks/better_errors.rb
@@ -1,3 +1,11 @@
+=begin
+
+    This file is part of the Arachni::Debug::Rack project and may be subject to
+    redistribution and commercial restrictions. Please see the Arachni::Debug::Rack
+    web site for more information on licensing and terms of use.
+
+=end
+
 fail 'Not implemented yet.'
 
 require 'better_errors'
--- lib/arachni/debug/rack/version.rb
+++ lib/arachni/debug/rack/version.rb
@@ -1,7 +1,17 @@
+=begin
+
+    This file is part of the Arachni::Debug::Rack project and may be subject to
+    redistribution and commercial restrictions. Please see the Arachni::Debug::Rack
+    web site for more information on licensing and terms of use.
+
+=end
+
 module Arachni
 module Debug
 module Rack
+
     VERSION = '0.1.0'
+
 end
 end
 end
